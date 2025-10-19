457fa9e42819b58fec1f96466aa5aa0f20c58d39
NguyenXuanVinh <nongami87@gmail.com>
2025-10-19 10:33:34 +0700

    style(app): reorganizes module imports for clarity
    - improves readability and maintainability by grouping and ordering module imports consistently.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,27 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
-import { PrismaModule } from './modules/prisma/prisma.module';
-import { UsersModule } from './modules/users/users.module';
-import { ProfilesModule } from './modules/profiles/profiles.module';
-import { ServersModule } from './modules/servers/servers.module';
-import { ChannelsModule } from './modules/channels/channels.module';
-import { RolesModule } from './modules/roles/roles.module';
-import { MessagesModule } from './modules/messages/messages.module';
 import { AttachmentsModule } from './modules/attachments/attachments.module';
+import { ChannelsModule } from './modules/channels/channels.module';
+import { MessagesModule } from './modules/messages/messages.module';
+import { PrismaModule } from './modules/prisma/prisma.module';
+import { ProfilesModule } from './modules/profiles/profiles.module';
+import { RolesModule } from './modules/roles/roles.module';
+import { ServersModule } from './modules/servers/servers.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule, ProfilesModule, ServersModule, ChannelsModule, RolesModule, MessagesModule, AttachmentsModule],
   controllers: [AppController],
+  imports: [
+    PrismaModule,
+    UsersModule,
+    ProfilesModule,
+    ServersModule,
+    ChannelsModule,
+    RolesModule,
+    MessagesModule,
+    AttachmentsModule,
+  ],
   providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(
-    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+    new ValidationPipe({ forbidNonWhitelisted: true, whitelist: true }),
   );
 
   await app.listen(process.env.PORT ?? 3000);
