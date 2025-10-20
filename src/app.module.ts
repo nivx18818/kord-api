115aad9f2f0bff5b2bf79a212955dca0019833f5
NguyenXuanVinh <nongami87@gmail.com>
2025-10-20 15:43:50 +0700

    style(app): reorder module imports for consistency
    - improves readability by organizing module imports in a more logical order.
    - no functional changes introduced.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AttachmentsModule } from './modules/attachments/attachments.module';
 import { ChannelsModule } from './modules/channels/channels.module';
+import { MembershipsModule } from './modules/memberships/memberships.module';
 import { MessagesModule } from './modules/messages/messages.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
 import { ProfilesModule } from './modules/profiles/profiles.module';
+import { ReactionsModule } from './modules/reactions/reactions.module';
 import { RolesModule } from './modules/roles/roles.module';
 import { ServersModule } from './modules/servers/servers.module';
 import { UsersModule } from './modules/users/users.module';
-import { MembershipsModule } from './modules/memberships/memberships.module';
-import { ReactionsModule } from './modules/reactions/reactions.module';
 
 @Module({
   controllers: [AppController],
