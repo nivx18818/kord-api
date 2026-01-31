ecab7f42b574babc16e593d313018b46ea1fd3f9
NguyenXuanVinh <nongami87@gmail.com>
2026-01-31 17:26:42 +0700

    feat(prisma): add graceful shutdown
    - implements lifecycle hook for disconnecting the database client on module destroy, ensuring proper resource cleanup.

--- src/modules/prisma/prisma.service.ts
+++ src/modules/prisma/prisma.service.ts
@@ -1,8 +1,15 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from 'generated/prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleDestroy, OnModuleInit
+{
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
